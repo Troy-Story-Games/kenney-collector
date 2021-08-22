4f66e4d2050188a83eb0d09ccbdf41a404a17e27
BingBongBang <travis.constantino@gmail.com>
2021-08-21 17:52:43 -0700

    Level Design

--- Game.gd
+++ Game.gd
@@ -6,10 +6,10 @@ onready var spawn = $Conveyor/Spawn
 
 
 func _ready():
-    Music.play("Menu")
-    Utils.initialize_vr()
+	Music.play("Menu")
+	Utils.initialize_vr()
 
 
 func _on_Timer_timeout():
-    var instance : RigidBody = Utils.instance_scene_on_main(crate, spawn.global_transform)
-    instance.apply_central_impulse(Vector3(5, 0, 0))
+	var instance : RigidBody = Utils.instance_scene_on_main(crate, spawn.global_transform)
+	instance.apply_central_impulse(Vector3(5, 0, 0))
